408b78047fccd00737b145456f4cb757f48ae581
GeorgiiGalechyan <galechyan1991@gmail.com>
2022-11-01 23:18:14 +0400

    Проблема с состоянием чекбокса решенаgit add .!

--- client/src/components/Forms/AuthForm/AuthForm.jsx
+++ client/src/components/Forms/AuthForm/AuthForm.jsx
@@ -10,11 +10,14 @@ import classes from './AuthForm.module.css'
 
 export const AuthForm = ({ handlers }) => {
   // Отлавиваем состояние input элементов
-  const [authData, setAuthData] = useState({ login: '', password: '', remember: '' })
+  const [authData, setAuthData] = useState({ login: '', password: '', isRemember: true })
 
-  // Связываем введенные в input данные со state (authData)
   const changeValue = (event) => {
-    const { id, value } = event.target
+    let { id, value, type, checked = '' } = event.target
+    if (type === 'checkbox') {
+      value = checked
+    }
+
     setAuthData((prevAuthData) => ({ ...prevAuthData, [id]: value }))
   }
 
@@ -30,11 +33,16 @@ export const AuthForm = ({ handlers }) => {
       <form onSubmit={(e) => e.preventDefault()} className={classes.authForm}>
         <TextInput id="login" placeholder="Login/E-mail/Phone" value={authData.login} onChange={changeValue} />
         <PassInput id="password" placeholder="Password" value={authData.password} onChange={changeValue} />
-        <CheckInput id="remember" value={authData.remember} onChange={changeValue} label="Remember me" />
+        <CheckInput
+          id="isRemember"
+          value={authData.isRemember}
+          onChange={changeValue}
+          checked={authData.isRemember}
+          label="Remember me"
+        />
         <Button title="Sign in" onClick={signIn} />
         <Button title="Forgot password?" onClick={handlers} />
       </form>
     </div>
   )
 }
-
