911b9d678c15610b2336f48315977377c0a8467e
GeorgiiGalechyan <galechyan1991@gmail.com>
2022-11-07 03:11:54 +0400

    Осваиваю react-router-dom

--- client/src/app.js
+++ client/src/app.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 // Components
 import { Header } from './components/Header/Header'
 import { Screen } from './components/Screen/Screen'
 import { Footer } from './components/Footer/Footer'
 
 // Screens (pages) - Routes
-import { StartScreen } from './components/Screen/Contents/StartPage/StartScreen'
-import { ListOfGames } from './components/Screen/Contents/ListOfGames/ListOfGames'
-import { Rating } from './components/Screen/Contents/Rating/Rating'
-import { PlayerStatistic } from './components/Screen/Contents/PlayerStatistic/PlayerStatistic'
+import { StartScreen } from './components/Screen/StartPage/StartScreen'
+import { ListOfGames } from './components/Screen/ListOfGames/ListOfGames'
+import { Rating } from './components/Screen/Rating/Rating'
+import { PlayerStatistic } from './components/Screen/PlayerStatistic/PlayerStatistic'
 
 // Styles
 import './App.css'
@@ -19,6 +18,14 @@ export const App = () => {
   return (
     <div className="App">
       <Router>
+        {/*Navbar*/}
+        <Routes>
+          <Route path="/" element={<StartScreen />} />
+          <Route path="/games" element={<ListOfGames />} />
+          <Route path="/rating" element={<Rating />} />
+          <Route path=`/${id || null}/statistics` element={<PlayerStatistic />} />
+          <Route path="*" element={<h1>Error</h1>} />
+        </Routes>
         {/*<Navbar/>*/}
         {/* <Routes> */}
         <Header />
